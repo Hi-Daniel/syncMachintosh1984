5f3356e52b6552e0bc38bbcf2f2be276e0602405
Daniel Jimenez Gil <dannyj.g@outlook.es>
2024-02-02 21:36:31 -0500

    added main file with pseudo code

---
+++ main.ino
@@ -0,0 +1,25 @@
+/*
+syncMachintosh1984
+by: Daniel Jimenez Gil
+license: MIT
+doc: main script going into ESP32
+*/
+
+void setup()
+{
+    // connect to WiFi
+    // begin mqtt connection
+    // begin TFT paint application
+}
+
+void loop()
+{
+    // stand-by mode
+    //   - check mqtt for data
+    //   - check lcd touch input
+
+    // active mode
+    //   - respond to user input
+    //   - send paint data to mqtt every X-changes
+    //   - set stand-by mode
+}
